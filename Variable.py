67745262c15449fc7b68c8170e176bbf2d227884
Arkhh1 <neznauhihi@gmail.com>
2024-10-23 16:15:49 +0300

    Add files via upload

---
+++ Variable.py
@@ -0,0 +1,5 @@
+Number_of_homework_completed = 12
+Number_of_hours_spent = 1.5
+Course_name = 'Pyton'
+Time_per_task = Number_of_hours_spent / Number_of_homework_completed
+print('Количество выполненных ДЗ:',Number_of_homework_completed,'Количество затраченных часов:',Number_of_hours_spent,'Название курса:',Course_name,'Время на одно задание:',Time_per_task)
